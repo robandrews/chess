003ee25ee61a7a6d2c9f164e263d7dcedc97b842
Andrew & Rob <rob.andrews@alumni.stanford.edu>
2014-02-04 15:12:43 -0800

    add Knight and King pieces

--- chess.rb
+++ chess.rb
@@ -24,15 +24,11 @@ class SlidingPiece < Piece
     self.move_dirs.each do |dir|
       test_space = self.pos
 
-      puts "Workign on: #{dir}"
       while @board.valid?(test_space)
         i, j = dir
         x, y = test_space
 
         if @board.empty?(test_space) # || other color piece
-          puts "Testing: #{test_space}"
-          puts "self pos vs testspace#{self.pos == test_space}"
-          puts "empty ? #{@board.empty?(test_space)}"
           possible_moves << test_space unless possible_moves.include? test_space
           test_space = [x+i, y+j]
         elsif test_space == self.pos
@@ -49,6 +45,25 @@ class SlidingPiece < Piece
 
 end
 
+class SteppingPiece < Piece
+  KING = [[1,1], [1,0], [1,-1], [0,-1], [-1,-1], [-1,0], [-1,1], [0,1]]
+  KNIGHT = [[1,2], [2,1], [-1,2], [2,-1], [-2,1], [1, -2], [-1,-2], [-2,-1]]
+
+  def moves
+    possible_moves = []
+    self.move_dirs.each do |dir|
+      i,j = dir
+      x,y = self.pos
+      test_space = [x+i, y+j]
+      if @board.valid?(test_space) && @board.empty?(test_space)
+        possible_moves << test_space
+      end
+    end
+    possible_moves
+  end
+end
+
+
 class Rook < SlidingPiece
 
   def move_dirs
@@ -56,7 +71,6 @@ class Rook < SlidingPiece
   end
 
 end
-
 class Bishop < SlidingPiece
 
   def move_dirs
@@ -64,7 +78,6 @@ class Bishop < SlidingPiece
   end
 
 end
-
 class Queen < SlidingPiece
 
   def move_dirs
@@ -73,7 +86,17 @@ class Queen < SlidingPiece
 
 end
 
+class King < SteppingPiece
+  def move_dirs
+    SteppingPiece::KING
+  end
+end
 
+class Knight < SteppingPiece
+  def move_dirs
+    SteppingPiece::KNIGHT
+  end
+end
 
 ### TODO
 
@@ -90,7 +113,15 @@ b[[4,4]] = bishop
 queen = Queen.new(:black, [3,5], b)
 b[[3,5]] = queen
 
+king = King.new(:black, [3,4], b)
+b[[3,4]] = king
+
+knight = Knight.new(:black, [5,6], b)
+b[[5,6]] = knight
+
 puts "Valid Rook Moves: #{rook.moves}"
 puts "Valid Bishop Moves: #{bishop.moves}"
 puts "Valid Queen Moves: #{queen.moves}"
+puts "Valid King Moves: #{king.moves}"
+puts "Valid Knight Moves: #{knight.moves}"
 
